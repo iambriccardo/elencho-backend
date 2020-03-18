a1b240087e7392ef348bfffed2f004907fa8f3a6
RiccardoBusetti <riccardob36@gmail.com>
2020-03-18 18:04:20 +0100

    Change worker interval from daily to weekly

--- cmd/elencho-scraper-worker/main.go
+++ cmd/elencho-scraper-worker/main.go
@@ -9,18 +9,19 @@ import (
 	"time"
 )
 
+const oneWeekHours = 168
+
 func main() {
-	tick := time.NewTicker(time.Hour * 24)
+	tick := time.NewTicker(time.Hour * oneWeekHours)
 	done := make(chan bool)
 	go scheduler(tick, done)
 	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 	<-sigs
 	done <- true
 }
 
 func scheduler(tick *time.Ticker, done chan bool) {
-	task()
 	for {
 		select {
 		case <-tick.C:
@@ -33,9 +34,12 @@ func scheduler(tick *time.Ticker, done chan bool) {
 
 func task() {
 	db := elencho.Make()
-	db.Open()
+	err := db.Open()
+	if err != nil {
+		fmt.Printf("an error occurred while running the scraper: %q", err)
+	}
 	defer db.Close()
-	err := elencho.Start(db)
+	err = elencho.Start(db)
 	if err != nil {
 		fmt.Printf("an error occurred while running the scraper: %q", err)
 	}
