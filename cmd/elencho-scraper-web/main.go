6061fd340ff3142ae47d3be1c10371f65b161700
Riccardo Busetti <riccardob36@gmail.com>
2021-10-24 12:16:51 +0200

    Add CORS support

--- cmd/elencho-scraper-web/main.go
+++ cmd/elencho-scraper-web/main.go
@@ -19,6 +19,7 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.Use(CORSMiddleware())
 
 	poolSize, err := el.GetIntEnv("POOL_SIZE")
 	if err != nil {
@@ -64,6 +65,22 @@ func main() {
 	if err != nil {
 		close(pool)
 		log.Fatalf("an error occurred in web: %q", err)
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
 
